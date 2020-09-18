a959594f4bde296b741c96d40c3e4d7c8504ff69
jose120918 <jose.esteban0918@gmail.com>
2020-09-18 16:29:10 -0500

    Write a program that prints the numbers from 00 to 99

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int i = 0;
 
-	for(i = 0; i < 100 ; i++)
+	for (i = 0; i < 100 ; i++)
 	{
-		putchar((i/10)+'0');
-		putchar((i%10)+'0');
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
 		if (i == 99)
 		{
 			putchar('\n');
-			return(0);
+			return (0);
 		}
 		putchar(',');
 		putchar(' ');
