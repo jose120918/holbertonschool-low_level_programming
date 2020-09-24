4fb1674032c8a7dd615b1c636161ccd6f1bee01e
jose120918 <jose.esteban0918@gmail.com>
2020-09-23 19:47:39 -0500

    Sumar dos enteros

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - Suma dos enteros por función.
+ * @i: Primera varable entera a sumar.
+ * @l: Segunda variable a sumar.
+ */
+void add(int i, int l)
+{
+	int n;
+	 n = i + l;
+	return (n);
+}
