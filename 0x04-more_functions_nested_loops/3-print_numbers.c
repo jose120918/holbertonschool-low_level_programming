1fd6e43543416498a2f786de9eb79342b72d0795
jose120918 <jose.esteban0918@gmail.com>
2020-09-24 20:44:14 -0500

    Write a function that prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - checks for lowercase character.
+ * @a: entero recibo por main.
+ * @b: entero recibido por main
+ * Return: 1 if c is uppercase
+ */
+void print_numbers(void);
+{
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
