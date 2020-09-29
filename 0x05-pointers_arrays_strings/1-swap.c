188c7a2285d4bd4ae77e230b91ea48d8a523ff85
jose120918 <jose.esteban0918@gmail.com>
2020-09-29 14:00:34 -0500

    Write a function that swaps the values of two integers.

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * reset_to_98 - main function.
- * @n: Pointer with the address value.
- *
+ * swap_int - main function.
+ * @a: Pointer with the address value.
+ * @b: Pointer with the address value.
  * Description: This function takes a pointer to an int as parameter
  * and updates the value it points to to 98.
  *
