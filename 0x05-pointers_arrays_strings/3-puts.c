beb8dcaefcbc16b913c671109c14ebf63ff5c9f2
jose120918 <jose.esteban0918@gmail.com>
2020-09-30 23:39:33 -0500

    Prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _puts - main function.
+ * @str: Pointer with the address value.
+ * Description: This function takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ *
+ * Return: none.
+ */
+void _puts(char *str)
+{
+	return (str);
+}
