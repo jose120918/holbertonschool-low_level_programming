6f93abfa772494246e7ec2a97276e907ce3e5633
jose120918 <jose.esteban0918@gmail.com>
2020-10-06 00:26:01 -0500

    Unir dos malditos arrays

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strcat - main function.
+ * @dest: Pointer with the address value.
+ * @src: Pointer with the adress value.
+ * Description: Write a function that concatenates two strings.
+ *
+ * Return: none.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int l = 0, i = 0;
+	while (dest[l] != '\0')
+	{
+		l++;
+	}
+	while (src[i] != '\0')
+	{
+		dest[l] = src [i];
+		l++;
+		i++;
+	}
+	dest[l] = '\0';
+	return (dest);
+}
