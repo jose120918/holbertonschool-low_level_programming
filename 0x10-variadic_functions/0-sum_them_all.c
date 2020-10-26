535bf67d41922f32540c3cf80d38f9679c605c62
jose120918 <jose.esteban0918@gmail.com>
2020-10-25 23:38:11 -0500

    second commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,26 +3,22 @@
 
 /**
  * sum_them_all: Funcion caca
- * 
+ *
  * @n: NUmero de argumentos a sumar
- * 
+ *
  * return: salida
- */
+*/
 
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list lista;
 	int suma = 0;
-	if (n != 0)
+
+	va_start(lista, n);
+	for (unsigned int i = 0; i < n; i++)
 	{
-		va_start (lista, n);
-		for (unsigned int i = 0; i < n; i++)
-		{
-			suma += va_arg(lista, int);
-		}
-		va_end(lista);
-		return (suma);
+		suma += va_arg(lista, int);
 	}
-	else
-		return (0);
+	va_end(lista);
+	return (suma);
 }
