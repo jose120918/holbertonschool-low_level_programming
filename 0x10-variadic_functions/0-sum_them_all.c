01f4d00aface653299b5cdc1f616a5f704fcc25c
jose120918 <jose.esteban0918@gmail.com>
2020-10-25 23:33:42 -0500

    first commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all: Funcion caca
+ * 
+ * @n: NUmero de argumentos a sumar
+ * 
+ * return: salida
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list lista;
+	int suma = 0;
+	if (n != 0)
+	{
+		va_start (lista, n);
+		for (unsigned int i = 0; i < n; i++)
+		{
+			suma += va_arg(lista, int);
+		}
+		va_end(lista);
+		return (suma);
+	}
+	else
+		return (0);
+}
