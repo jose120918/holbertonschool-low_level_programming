7e9957b75a3162f20455cfca17c249bfc709d97a
jose120918 <jose.esteban0918@gmail.com>
2020-11-14 23:23:45 -0500

    2 commit

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,5 +7,28 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	int nodes = 0;
+	const listint_t *turtle = head;
 
+	if (head == NULL)
+	{
+		return (0);
+	}
+	printf("[%p] %i\n", (void *)head, (*head).n);
+	head = (*head).next;
+	for (nodes = 0; head != NULL; nodes++)
+	{
+		if (head < turtle)
+		{
+			printf("[%p] %i\n", (void *)head, (*head).n);
+		}
+		else
+		{
+			printf("-> [%p] %i\n", (void *)head, (*head).n);
+			break;
+		}
+		head = (*head).next;
+		turtle = (*turtle).next;
+	}
+	return (nodes + 1);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,5 +7,28 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
+	int nodes = 0;
+	listint_t *turtle = h;
 
+	if (head == NULL)
+	{
+		return (0);
+	}
+	printf("[%p] %i\n", (void *)head, (*head).n);
+	head = (*head).next;
+	for (nodes = 0; head != NULL; nodes++)
+	{
+		if (head < turtle)
+		{
+			printf("[%p] %i\n", (void *)head, (*head).n);
+		}
+		else
+		{
+			printf("-> [%p] %i\n", (void *)head, (*head).n);
+			break;
+		}
+		head = (*head).next;
+		turtle = (*turtle).next;
+	}
+	return (nodes + 1);
 }
