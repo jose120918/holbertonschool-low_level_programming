e9dece6d9922d3244358d6dbd76b7f564457df3d
jose120918 <jose.esteban0918@gmail.com>
2020-12-14 12:11:09 -0500

    Print number of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - returns the number of elements in a linked
+ * @h: Head of the list
+ * Return: Number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
