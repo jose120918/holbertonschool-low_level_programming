4441e9cc4f75fad4409ada6a87181a522a7e2e74
jose120918 <jose.esteban0918@gmail.com>
2020-12-14 12:17:13 -0500

    new node at the end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - adds a new node at the end
+ * @h: Head of the list
+ * Return: new node at the end
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t)); /*New node*/
+	dlistint_t *node_aux = NULL; /*Auxiliar variable*/
+
+	if (!new)
+	{
+		return (NULL);
+	}
+
+	new->n = n; /*Pass the data to new node*/
+	new->next = NULL;
+
+	if (!*head)
+	{
+		new->prev = NULL;
+		*head = new;
+	}
+	else
+	{
+		node_aux = *head; /*Passing head value to auxiliar variable*/
+		while (node_aux->next) /*Moving into the list*/
+		{
+			node_aux = node_aux->next;
+		}
+		node_aux->next = new; /*Assign last auxiliar node*/
+		new->prev = node_aux; /*to the new node*/
+		new->next = NULL;
+	}
+	return (new);
+}
