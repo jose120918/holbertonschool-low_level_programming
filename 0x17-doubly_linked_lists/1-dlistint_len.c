865d40476e7d3868b07ac3fed3b9c767f2141ec9
jose120918 <jose.esteban0918@gmail.com>
2020-12-14 12:15:28 -0500

    number of elements

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,8 +1,18 @@
 #include "lists.h"
 
 /**
- * print_dlistint - returns the number of elements in a linked
+ * print_dlistint - returns the number of elements
  * @h: Head of the list
  * Return: Number of elements
  */
 size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0; /* Variable for counting elements*/
+
+	while (h) /* While list exist*/
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
