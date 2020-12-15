77b7ca74fd1cd919ad55c84a0bc8f4fe115915a9
jose120918 <jose.esteban0918@gmail.com>
2020-12-14 21:28:18 -0500

    Funciones

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * *_strstr - main function.
+ * @haystack: Pointer to the string to search.
+ * @needle: Pointer to the substring to find.
+ *
+ * Description: This function locates a substring.
+ *
+ * Return: A pointer to the beginning of the located substring, or
+ * NULL if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (0);
+}
