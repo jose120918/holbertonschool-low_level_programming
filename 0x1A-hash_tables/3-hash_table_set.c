11ca2344bedd4f9ebce056e84d5ee065ed50b398
jose120918 <jose.esteban0918@gmail.com>
2021-01-15 21:54:42 -0500

    .

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,61 +1,43 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_set - function that adds an element to the hash table.
- * @ht: the hash table you want to add or update
- * @key: is the key
- * @value:the value associated with the key.
- * Return: 1 if it succeeded, 0 if not
+ * hash_table_set - add element to the hash table
+ * @ht: hast to add
+ * @key: the key for the hash
+ * @value: value associated with the key
+ *
+ * Return: success or not
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int idx = 0;
-	hash_node_t *new = NULL;
+	hash_node_t *new = NULL, *list = NULL;
+	unsigned long int i = 0;
 
-	if (!ht || !key || !(*key) || !value)
+	if (!new || !list || !ht)
 		return (0);
-
-	idx = key_index((const unsigned char *)key, ht->size);
-
-	if (ht->array[idx] && !(match(key, ht->array[idx], value)))
-		return (1);
-
+	/* new space for the new element */
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
 		return (0);
+	/* index of the hash table */
+	i = key_index((const unsigned char *)key, ht->size);
+	/* hash table with i elements of the array */
+	list = ht->array[i];
+	while (list)
+	{ /* compare number of the key*/
+		if (strcmp(list->key, key) == 0)
+		{
+			free(list->value);
+			/* asigne the value*/
+			list->value = strdup(value);
+			return (1);
+		}
+		/* travel in the list */
+		list = list->next;
+	}
 	new->key = strdup(key);
 	new->value = strdup(value);
-	if (!(new->key) || !(new->value))
-	{
-		if (new->key)
-			free(new->key);
-		free(new);
-		return (0);
-	}
-	new->next = ht->array[idx];
-	ht->array[idx] = new;
-	return (1);
-}
-
-/**
- *match - Check if a key value is already in a bucket.
- * @key: key
- * @arry: bucket
- * @value: node value to update
- * Return: 1 if not match and 0 if match
- */
-int match(const char *key, hash_node_t *arry, const char *value)
-{
-	hash_node_t *elment = arry;
-
-	while (elment)
-	{
-		if (!(strcmp(elment->key, key)))
-		{
-			free(elment->value);
-			elment->value = strdup(value);
-			return (0);
-		}
-		elment = elment->next;
-	}
+	new->next = ht->array[i];
+	ht->array[i] = new;
 	return (1);
 }
